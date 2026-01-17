33d5ff7dc8f70a73b1e0823125133d2e300a38a2
hugo <hugoanguiano0327@gmail.com>
2026-01-17 17:37:19 +0100

    add pydantic schemas and improve api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app.models import player
 from app.routers import players_router
@@ -7,6 +8,14 @@ Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(title="TK3")
+# allow angular to do request
+app.add_middleware(
+      CORSMiddleware,
+      allow_origins=["http://localhost:4200"],  
+      allow_credentials=True,                   
+      allow_methods=["*"],                       
+      allow_headers=["*"],                       
+  )
 app.include_router(players_router.router)
 @app.get("/")
 def home():
